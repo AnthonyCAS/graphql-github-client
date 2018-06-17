54902f6c15551e93717e46e0d32024ea271cf95d
Anthony CA <anthony.ccapira@ucsp.edu.pe>
2018-06-16 23:40:27 -0500

    Adding callback AFTER_TEXT_CHANGED to searchBox, using a delay for a new query

--- app/src/main/java/com/github/zhira/githubgraphqlapp/activities/GithubUsersActivity.kt
+++ app/src/main/java/com/github/zhira/githubgraphqlapp/activities/GithubUsersActivity.kt
@@ -2,13 +2,45 @@ package com.github.zhira.githubgraphqlapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.github.zhira.githubgraphqlapp.R
+import butterknife.OnTextChanged
+import java.util.*
+import android.R.string.cancel
+
+
+
 
 class GithubUsersActivity : AppCompatActivity() {
+
+    @BindView(R.id.search_box) lateinit var searchEditText: EditText
+
+    var timer = Timer()
+    private val delay: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_github_users)
+        ButterKnife.bind(this)
+    }
+
+    // callback for searchBox, wait 1 second for a new query
+    @OnTextChanged(value = R.id.search_box, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    fun queryChanged(text: CharSequence) {
+        timer.cancel()
+        timer = Timer()
+        timer.schedule(
+                object : TimerTask() {
+                    override fun run() {
+                        Log.e("Some", text.toString())
+                    }
+                },
+                delay
+        )
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
