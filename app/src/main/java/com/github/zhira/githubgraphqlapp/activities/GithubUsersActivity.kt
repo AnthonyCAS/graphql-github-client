1d7a65aa642ba1659447430cde0fe1ae98e5436d
Anthony CA <anthony.ccapira@ucsp.edu.pe>
2018-06-17 00:24:57 -0500

    Creating custom user adapter with a user_item layout to show each user information into the recycler view

--- app/src/main/java/com/github/zhira/githubgraphqlapp/activities/GithubUsersActivity.kt
+++ app/src/main/java/com/github/zhira/githubgraphqlapp/activities/GithubUsersActivity.kt
@@ -2,29 +2,35 @@ package com.github.zhira.githubgraphqlapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.widget.EditText
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.github.zhira.githubgraphqlapp.R
 import butterknife.OnTextChanged
+import com.github.zhira.githubgraphqlapp.adapters.UserAdapter
 import java.util.*
-import android.R.string.cancel
-
-
-
 
 class GithubUsersActivity : AppCompatActivity() {
 
     @BindView(R.id.search_box) lateinit var searchEditText: EditText
+    @BindView(R.id.users_list_rv) lateinit var usersRecyclerView: RecyclerView
 
     var timer = Timer()
     private val delay: Long = 1000
+
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_github_users)
         ButterKnife.bind(this)
+        viewManager = LinearLayoutManager(this)
+        //viewAdapter = UserAdapter(myDataset)
     }
 
     // callback for searchBox, wait 1 second for a new query
---
+++ app/src/main/java/com/github/zhira/githubgraphqlapp/adapters/UserAdapter.kt
@@ -0,0 +1,10 @@
+package com.github.zhira.githubgraphqlapp.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import com.github.zhira.githubgraphqlapp.R
+
+class UserAdapter(private val dataset: Array<String>)  {
+}
