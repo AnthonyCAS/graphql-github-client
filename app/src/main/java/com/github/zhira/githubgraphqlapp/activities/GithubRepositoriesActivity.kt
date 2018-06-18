fd182d9b02ff2abc33c3b74d5d6dbcd9126bf596
Anthony CA <anthony.ccapira@ucsp.edu.pe>
2018-06-17 20:06:09 -0500

    Adding real repositories data to the recycler view

--- app/src/main/java/com/github/zhira/githubgraphqlapp/activities/GithubRepositoriesActivity.kt
+++ app/src/main/java/com/github/zhira/githubgraphqlapp/activities/GithubRepositoriesActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.github.zhira.githubgraphqlapp.R
 import com.github.zhira.githubgraphqlapp.adapters.RepositoryAdapter
 import com.github.zhira.githubgraphqlapp.models.Item
 import com.github.zhira.githubgraphqlapp.utilities.Constants
+import com.github.zhira.githubgraphqlapp.utilities.GraphQlTools
 import kotlinx.android.synthetic.main.activity_github_repositories.view.*
 import java.util.ArrayList
 
@@ -19,10 +25,15 @@ class GithubRepositoriesActivity : AppCompatActivity() {
     @BindView(R.id.repositories_toolbar) lateinit var toolbar: android.support.v7.widget.Toolbar
     @BindView(R.id.repositories_list_rv) lateinit var repositoryRecyclerView: RecyclerView
 
+    private lateinit var repositoryAdapter: RepositoryAdapter
+    private lateinit var client: ApolloClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_github_repositories)
         ButterKnife.bind(this)
+        client = GraphQlTools.setupApollo()
+
         val userLogin = intent.getStringExtra(Constants.LOGIN_USER_CODE)
         val userName = intent.getStringExtra(Constants.NAME_USER_CODE)
 
@@ -32,22 +43,35 @@ class GithubRepositoriesActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val items =  getLists()
         repositoryRecyclerView.layoutManager = LinearLayoutManager(this)
         repositoryRecyclerView.hasFixedSize()
-        repositoryRecyclerView.adapter = RepositoryAdapter(items)
+        repositoryAdapter = RepositoryAdapter()
+        repositoryRecyclerView.adapter = repositoryAdapter
         repositoryRecyclerView.addItemDecoration(DividerItemDecoration(
                 this,
                 DividerItemDecoration.VERTICAL))
+        if (userLogin != null)
+            loadUsers(userLogin, 50)
     }
 
-    fun getLists(): ArrayList<Item> {
-        var lists = ArrayList<Item>()
-        lists.add(Item("1", "Item 1", "Descripcion 1", "anthony"))
-        lists.add(Item("2", "Item 2", "Descripcion 2", "some"))
-        lists.add(Item("3", "Item 3", "Descripcion 3", "juan"))
-        lists.add(Item("4", "Item 4", "Descripcion 4", "tomas"))
-        return lists;
+    private fun loadUsers (login: String, limit: Int) {
+        client.query(SearchRepositoryQuery
+                .builder()
+                .login(login)
+                .number(limit)
+                .build())
+                .enqueue(object : ApolloCall.Callback<SearchRepositoryQuery.Data>() {
+                    override fun onFailure(e: ApolloException) {
+                        Log.e("Error from: ", e.message.toString())
+                    }
+                    override fun onResponse(response: Response<SearchRepositoryQuery.Data>) {
+                        runOnUiThread {
+                            repositoryAdapter.updateData(
+                                    response!!.data()!!.user()!!.repository().repositoryEntry() as List<SearchRepositoryQuery.RepositoryEntry>
+                            )
+                        }
+                    }
+                })
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/github/zhira/githubgraphqlapp/adapters/RepositoryAdapter.kt
+++ app/src/main/java/com/github/zhira/githubgraphqlapp/adapters/RepositoryAdapter.kt
@@ -13,17 +13,28 @@ import kotlinx.android.synthetic.main.adapter_repository_item.view.*
  * Repository Adapter
  * It helps to map data into the layout all repositories for the current selected user
  */
-class RepositoryAdapter(private val dataSet: ArrayList<Item>): RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
+class RepositoryAdapter(): RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
 
+    private val repositoryEntries: MutableList<SearchRepositoryQuery.RepositoryEntry>
+
+    init {
+        this.repositoryEntries = ArrayList()
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.adapter_repository_item, parent, false)
         return ViewHolder(view, parent.context)
     }
 
-    override fun getItemCount() = dataSet.size
+    override fun getItemCount() = repositoryEntries.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItems(dataSet[position])
+        holder.bindItems(repositoryEntries[position])
+    }
+
+    fun updateData(data: List<SearchRepositoryQuery.RepositoryEntry>) {
+        repositoryEntries.clear()
+        repositoryEntries.addAll(data)
+        notifyDataSetChanged()
     }
 
     /**
@@ -33,10 +44,12 @@ class RepositoryAdapter(private val dataSet: ArrayList<Item>): RecyclerView.Adap
      *  repository description
      */
     class ViewHolder(itemView: View, val context: Context): RecyclerView.ViewHolder(itemView) {
-        fun bindItems (item: Item) {
-            itemView.repository_name.text = item.login
-            itemView.repository_pr_count.text = "${context.resources.getString(R.string.github_repositories_pr_label)} ${item.location}"
-            itemView.repository_description.text = item.picture_url
+        fun bindItems (item: SearchRepositoryQuery.RepositoryEntry) {
+            itemView.repository_name.text = item.node()!!.name()
+            itemView.repository_pr_count.text = "${context.resources.getString(R.string.github_repositories_pr_label)} ${item.node()!!.pullRequests().totalCount()}"
+            itemView.repository_description.text = "No description."
+            if (item.node()!!.description()!= null)
+                itemView.repository_description.text = item.node()!!.description()
         }
     }
 }
